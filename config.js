63c86327e33c63cab67cbd28c9718f9db4178da2
tfkhdyt <tfkhdyt@pm.me>
2021-12-19 11:21:35 +0700

    nambah header

---
+++ config.js
@@ -0,0 +1,3 @@
+export default {
+  title: "TFKHDYT's Full Stack Roadmap",
+}
---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Config from '../config'
+import Header from './components/Header'
+import Roadmap from './components/Roadmap'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>{Config.title}</title>
+      </Head>
+      <div className='flex flex-col p-4'>
+        <Header />
+        <Roadmap />
+      </div>
+    </>
+  )
+}
