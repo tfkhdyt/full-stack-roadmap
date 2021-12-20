efb4d5fce343a06010635398678db51a526442a9
tfkhdyt <ta142fa@gmail.com>
2021-12-20 12:06:54 +0700

    nambah component metatags

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,15 @@ import Config from '../config'
 import Header from '../components/Header'
 import Roadmap from '../components/Roadmap'
 import BackToTop from '../components/BackToTop'
+import MetaTags from '../components/MetaTags'
 
 export default function Home() {
   return (
     <>
       <Head>
+        <MetaTags />
         <title>{Config.title}</title>
+        <link rel='shortcut icon' href={Config.favicon} />
       </Head>
       <div className='flex flex-col p-4 space-y-4'>
         <Header />
