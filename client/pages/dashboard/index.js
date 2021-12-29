650c6fa0731faeb6fc3aa7c9d79b2032a88d0008
tfkhdyt <ta142fa@gmail.com>
2021-12-29 17:12:56 +0700

    [client] fix gagal logout dan sembunyikan tombol pend di halaman detail untuk non-admin

--- client/pages/dashboard/index.js
+++ client/pages/dashboard/index.js
@@ -38,7 +38,7 @@ function Dashboard() {
   useEffect(() => {
     if (error) {
       if (error.status === 401) {
-        router.push('/auth/login')
+        router.push('/login')
       } else if (error.status === 500) {
         Alert.fire({
           icon: 'error',
@@ -60,7 +60,7 @@ function Dashboard() {
         title: 'Logout success!',
       })
       cookies.remove('token', { path: '/' })
-      router.push('/dashboard')
+      router.push('/login')
     }
   }
 
