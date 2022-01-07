1c94ee71af65d5a0875c3f4aef4cc667be2aa35c
tfkhdyt <ta142fa@gmail.com>
2022-01-07 18:27:26 +0700

    [client] fix gtag script

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -7,15 +7,14 @@ function MyApp({ Component, pageProps }) {
       {/* Global site tag (gtag.js) - Google Analytics */}
       <Script
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-        strategy='lazyOnload'
       />
-      <Script strategy='lazyOnload'>
+      <Script>
         {`
-              window.dataLayer = window.dataLayer || []
-              const gtag = () => dataLayer.push(arguments)
-              gtag('js', new Date())
-              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}')
-            `}
+          window.dataLayer = window.dataLayer || []
+          const gtag = (...arguments) => dataLayer.push(arguments)
+          gtag('js', new Date())
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}')
+        `}
       </Script>
       <Component {...pageProps} />
     </>
