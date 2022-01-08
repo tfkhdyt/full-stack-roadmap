dd1d47d79b493b60c273d926320e56e6323a8a68
tfkhdyt <ta142fa@gmail.com>
2022-01-08 12:32:10 +0700

    [client] fix error pada spellCheck

--- client/components/TextAreaForm.jsx
+++ client/components/TextAreaForm.jsx
@@ -8,7 +8,7 @@ function TextAreaForm({ label, id, onChange, ...rest }) {
         name={id}
         id={id}
         className='w-full py-2 px-3 h-28 rounded-md outline-none focus:ring-2 focus:ring-blue-400 transition duration-200 ease-in-out bg-gray-700 text-gray-200 resize-none block'
-        spellcheck
+        spellCheck
         onChange={onChange}
         {...rest}
         required
